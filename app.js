3b3f180a81366e7893b98c1ff9b4900a0d8df485
millennialmoron <millennialmoron87@gmail.com>
2022-05-28 17:45:43 +0200

    added some saltiness

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const SHA256 = require("crypto-js/sha256");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,16 +35,23 @@ async function main() {
     })
     .post(function (req, res) {
       const username = req.body.username;
-      const password = SHA256(req.body.password).toString();
-
+      const password = req.body.password;
       User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
           console.log(err);
         } else {
           if (foundUser) {
-            if (foundUser.password === password) {
-              res.render("secrets.ejs");
-            }
+            bcrypt.compare(
+              password,
+              foundUser.password,
+              function (err, result) {
+                if (result === true) {
+                  res.render("secrets.ejs");
+                } else {
+                  console.log("i dunno man, something bad happened..." + err);
+                }
+              }
+            );
           }
         }
       });
@@ -55,18 +63,21 @@ async function main() {
       res.render("register.ejs");
     })
     .post(function (req, res) {
-      const newUser = new User();
-      newUser.email = req.body.username;
-      newUser.password = SHA256(req.body.password).toString();
-      newUser.save(function (err) {
-        if (err) {
-          console.log("AHHH PANIC!!" + err);
-        } else {
-          res.render("secrets.ejs");
-        }
+      bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User();
+        newUser.email = req.body.username;
+        newUser.password = hash;
+        newUser.save(function (err) {
+          if (err) {
+            console.log("AHHH PANIC!!" + err);
+          } else {
+            res.render("secrets.ejs");
+          }
+        });
       });
     });
 }
+
 app.listen(3000, function () {
   console.log("am listenin'");
 });
