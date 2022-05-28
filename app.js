52bbc5c8ecae273d97d3dbd84c6875ec433a6b05
millennialmoron <millennialmoron87@gmail.com>
2022-05-28 23:32:36 +0200

    added google oauth

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -36,12 +38,37 @@ async function main() {
   });
 
   userSchema.plugin(passportLocalMongoose);
+  userSchema.plugin(findOrCreate);
 
   const User = new mongoose.model("User", userSchema);
 
   passport.use(User.createStrategy());
-  passport.serializeUser(User.serializeUser());
-  passport.deserializeUser(User.deserializeUser());
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleID: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    )
+  );
 
   app.get("/", function (req, res) {
     res.render("home.ejs");
@@ -80,6 +107,19 @@ async function main() {
         res.redirect("/secrets");
       }
     );
+
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+  );
+
+  app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+      res.redirect("/secrets");
+    }
+  );
 
   app.get("/secrets", function (req, res) {
     if (req.isAuthenticated()) {
